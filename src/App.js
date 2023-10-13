4370d5de494c2c5ecf5b51ae5f048e3ee75a0e62
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-10-13 14:45:35 +0100

    made final change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Name from './Components/Name';
+import Price from './Components/Price';
+import Description from './Components/Description';
+import Image from './Components/Image';
 
 function App() {
+  let firstName = 'Oluwatofunmi';
+  firstName = true ? <Image/> && firstName : false;
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className='border border-primary border-5 border-opacity-50 rounded' style={{ width: '18rem', margin: '50px', padding: '20px' }}>
+      <Image />
+      <Card.Body>
+        <Card.Title><Name/></Card.Title>
+        <Card.Text>
+          <Description/>
+        </Card.Text>
+        <Card.Text>
+          <Price/>
+        </Card.Text>
+        <Button variant="primary">Buy Now</Button>
+      </Card.Body>
+    </Card>
+
+    <h1 className='m-5'>Hello {firstName}</h1>
     </div>
   );
 }
---
+++ src/Components/Product.js
@@ -0,0 +1,9 @@
+import phone from './Media/ip.jpg';
+
+export const product = {
+    name: "iPhone X",
+    price: 29.99,
+    description: "The iPhone X featured a 5.8-inch Super Retina OLED display with a resolution of 1125 x 2436 pixels. This display was known for its vibrant colors, deep blacks, and HDR support.",
+    image: phone
+};
+
